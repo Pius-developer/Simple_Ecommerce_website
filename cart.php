7f407b5a4ab1787a0ed8187a5d14d972264dfda6
Pius_dev <105377990+Pius-developer@users.noreply.github.com>
2023-09-25 09:52:13 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -36,7 +36,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>My Cart</title>
 
-
 		<!-- Boostrap cdn-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
